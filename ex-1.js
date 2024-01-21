5a59ba686a46b84d89e00d8bdeb4680bc44fb36f
T <itthi91@gmail.com>
2024-01-21 17:13:14 +0700

    feat(assignment): submit

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,29 @@
-class EmailNotification { }
+class EmailNotification {
+    constructor(notificationId, createdTime, content, receiver) {
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.receiver}`)
+    }
+}
 
-class SMSNotification { }
+class SMSNotification {
+    constructor(notificationId, createdTime, content, phoneNumber) {
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.phoneNumber = phoneNumber;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.phoneNumber}`)
+    }
+}
+
+const test1 = new EmailNotification("1", "10-october-2023","this is a test", "test1@gmail.com")
+test1.send();
+
+const test2 = new SMSNotification("2","11-october-2023", "this is test2", "0xx-xxx-xxxx")
+test2.send();
--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,74 @@
 //  Start coding here
+class User {
+  constructor(id, name, email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+}
+
+class PostList {
+  constructor(posts) {
+    this.posts = [];
+  }
+  addPost(post) {
+    this.posts.push(post);
+  }
+  sharePost(title) {
+    console.log(`You've shared post ${title}} to your friend.`);
+  }
+}
+
+class Post {
+  constructor(id, title, content) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.comment = [];
+  }
+  addComment(comment) {
+    this.comment.push(comment);
+  }
+}
+
+class Comment {
+  constructor(id, content, createdBy, like) {
+    this.id = id;
+    this.content = content;
+    this.createdBy = createdBy;
+    this.like = 0;
+  }
+  addLike() {
+    this.like += 1;
+  }
+}
+
+class Facebook {
+  constructor() {
+    this.groupList = [];
+    this.pageList = [];
+  }
+  addGroup(group) {
+    this.groupList.push(group);
+  }
+  addPage(page) {
+    this.pageList.push(page);
+  }
+}
+
+class FacebookGroup {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Notification {
+  constructor(id) {
+    this.id = id;
+  }
+  send() {
+    console.log(
+      `Notification: ${createdBy} has just commented on this post—${title}`
+    );
+  }
+}
